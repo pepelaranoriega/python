f650ab3d2cf4974d4a69d05d0ff510168c1de721
pepelaranoriega <143450827+pepelaranoriega@users.noreply.github.com>
2025-05-10 23:21:52 -0700

    Ejercicio 6 - promedio de 5 calificaciones

---
+++ ejercicio6.py
@@ -0,0 +1,8 @@
+num1 = float(input("Calificacion primer periodo: "))
+num2 = float(input("Calificacion segundo periodo: "))
+num3 = float(input("Calificacion tercer periodo: "))
+num4 = float(input("Calificacion cuarto periodo: "))
+num5 = float(input("Calificacion quinto periodo: "))
+total = num1+num2+num3+num4+num5
+resultado = print(f"La calificacion final es: {total/5}")
+
