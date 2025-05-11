018426863656e43b42150eba7527b90f54c6b909
pepelaranoriega <143450827+pepelaranoriega@users.noreply.github.com>
2025-05-10 21:39:33 -0700

    Ejercicio 4 - Numero mayor

---
+++ ejercicio4.py
@@ -0,0 +1,20 @@
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+num3 = float(input("Ingrese el tercer número: "))
+
+<<<<<<< HEAD
+if num1 >= num2 and num1 >= num3:
+    print(f"El mayor es: {num1}")
+elif num2 >= num1 and num2 >= num3:
+    print(f"El mayor es: {num2}")
+else:
+    print(f"El mayor es: {num3}")
+=======
+if num1 >= num2 and num3 <= num2:
+    print (f"El mayor es: {num1}")
+elif num2 >= num1 and num3 <= num1:
+    print (f"El mayor es: {num2}")
+else:
+    print (f"El mayor es: {num3}")
+>>>>>>> fccc0eb (Ejercicio 4 - Sacar resultado de numero mayor)
+
