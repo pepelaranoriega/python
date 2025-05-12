eadfbbce2127765eb2543dab7b62b2f3a96fafdc
pepelaranoriega <143450827+pepelaranoriega@users.noreply.github.com>
2025-05-11 19:55:02 -0700

    Ejercicio 13 - contador de caracteres con LEN

---
+++ ejercicio13.py
@@ -0,0 +1,7 @@
+texto = input("Ingrese un texto: ")
+contador = len(texto)
+if contador > 10:
+    print(f"El texto es demasiado largo, tiene {contador} caracteres")
+else:
+    print(f"longitud del texto es de {contador}")
+
